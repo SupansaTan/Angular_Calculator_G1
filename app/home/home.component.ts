a81ab5025101fd3113ef4c9c7ab327f30077ca60
Supansa Tantulset <s6201012620236@email.kmutnb.ac.th>
2021-03-21 03:48:34 +0700

    Add methods for clear, backspace, number and operator buttons

--- app/home/home.component.ts
+++ app/home/home.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from "@angular/core";
-import { TextField } from "tns-core-modules/ui/text-field";
+import { Component, OnInit, ViewChild, ElementRef } from "@angular/core";
+import { EventData } from "@nativescript/core";
+import { Label } from "tns-core-modules/ui/label";
+import { Button } from "tns-core-modules/ui/button";
 
 @Component({
     selector: "Home",
@@ -8,15 +10,14 @@ import { TextField } from "tns-core-modules/ui/text-field";
     styleUrls: ["./home.component.css"]
 })
 export class HomeComponent implements OnInit {
-    onButtonTap(): void {
-        console.log("Button was pressed");
-    }
-
+    @ViewChild("display_num", { static: false }) expLabel: ElementRef;
+    @ViewChild("display_result", { static: false }) resultLabel: ElementRef;
 
     constructor() {
     }
 
     ngOnInit(): void {
+
     }
 
     precedence(op) {
@@ -35,5 +36,36 @@ export class HomeComponent implements OnInit {
         if (op == '-') { return num1 - num2 }
         if (op == 'x') { return num1 * num2 }
         if (op == '/') { return num1 / num2 }
+    }
+
+    clear_btn() {
+        this.expLabel.nativeElement.text = "0";
+        this.resultLabel.nativeElement.text = "=";
+    }
+
+    backspace_btn() {
+        let text_label = this.expLabel.nativeElement.text;
+        let slice_text = text_label.slice(0, text_label.length - 1);
+
+        if (slice_text == "" || text_label == "0") {
+            this.expLabel.nativeElement.text = "0";
+        }
+        else {
+            this.expLabel.nativeElement.text = slice_text;
+        }
+    }
+
+    pressed_btn(args: EventData) {
+        var button = <Button>args.object;
+        var value = button.text;
+        var text_label = this.expLabel.nativeElement.text;
+        var result_label = this.resultLabel.nativeElement.text;
+
+        if (text_label == "0") {
+            this.expLabel.nativeElement.text = value;
+        }
+        else {
+            this.expLabel.nativeElement.text = text_label + value;
+        }
     }
 }
