0c59dd1be3f561a9301023856f629e5d6bfcfeda
Supansa <s6201012620236@email.kmutnb.ac.th>
2021-03-21 19:57:20 +0700

    Add method evaluate the expresstion and power of two

--- app/home/home.component.ts
+++ app/home/home.component.ts
@@ -55,7 +55,7 @@ export class HomeComponent implements OnInit {
         }
     }
 
-    pressed_btn(args: EventData) {
+    addExp(args: EventData) {
         var button = <Button>args.object;
         var value = button.text;
         var text_label = this.expLabel.nativeElement.text;
@@ -66,6 +66,76 @@ export class HomeComponent implements OnInit {
         }
         else {
             this.expLabel.nativeElement.text = text_label + value;
+        }
+    }
+
+    evaluate() {
+        var num1, num2, op;
+        var values = [];
+        var operators = [];
+        var i = 0; // start index
+        var expression = this.expLabel.nativeElement.text;
+
+        while (i < expression.length) {
+            // if current expression is numeric
+            if (!isNaN(expression[i])) {
+                var num = "";
+
+                while (i < expression.length && (!isNaN(expression[i]) || expression[i] == '.')) {
+                    // current expression is numeric or decimal point
+                    num += expression[i];
+                    i++;
+                }
+                values.push(parseFloat(num));
+                i--;
+            }
+
+            // if current expression is operator
+            else {
+                // when have operator in operators array
+                while (operators.length != 0 && this.precedence(operators[operators.length - 1]) >= this.precedence(expression[i])) {
+                    num2 = values.pop();
+                    num1 = values.pop();
+                    op = operators.pop();
+                    values.push(this.calculate(num1, num2, op));
+                }
+
+                // not have operator in operators array 
+                operators.push(expression[i]);
+            }
+            i++;
+        }
+        // if remain operator and number in array
+        while (operators.length != 0 && (values.length > 1)) {
+            num2 = values.pop();
+            num1 = values.pop();
+            op = operators.pop();
+            values.push(this.calculate(num1, num2, op));
+        }
+        this.resultLabel.nativeElement.text = "= " + values[values.length - 1]; // show result
+    }
+
+    pow_btn() {
+        var result;
+        var exp_label = this.expLabel.nativeElement.text;
+        var result_label = this.resultLabel.nativeElement.text;
+
+        if (result_label == '=') { // if never press evaluate button
+            // get number
+            let numbers = exp_label.split('x').join(',').split('+').join(',').split('-').join(',').split('÷').join(',');
+            let list_numbers = numbers.split(",");
+            let last_number = parseFloat(list_numbers.pop());
+
+            result = Math.pow(last_number, 2);
+            let slice_text = exp_label.slice(0, exp_label.length - last_number.toString().length);
+            this.expLabel.nativeElement.text = slice_text + result.toString();
+        }
+        else {
+            // ever pressed evaluate button
+            let result_label_number = parseFloat(result_label.slice(2, result_label.length))
+            result = Math.pow(result_label_number, 2);
+            this.expLabel.nativeElement.text = '' + result;
+            this.resultLabel.nativeElement.text = '= ' + result;
         }
     }
 }
